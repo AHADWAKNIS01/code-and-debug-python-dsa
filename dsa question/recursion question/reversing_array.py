b4a6d7256a08f9183e970af95eebcb873ab0e4c8
ahad waknis <ahadwaknis666@gmail.com>
2026-09-06 11:57:43 +0530

    reverse number using the for loop

--- dsa question/recursion question/reversing_array.py
+++ dsa question/recursion question/reversing_array.py
@@ -1,14 +1,28 @@
 num=[1,2,34,5,65,7,7,88,9,]
 
+# #swaping the index
+# def func(num,left,right):
+#     if left>=right:
+#         return
+#     else:
+#         num[left], num[right]=num[right], num[left]
+#         func(num,left+1,right-1)
 
-def func(num,left,right):
-    if left>=right:
-        return
-    else:
-        num[left], num[right]=num[right], num[left]
-        func(num,left+1,right-1)
+# func(num,0,len(num)-1)
+# print(num)
 
-func(num,0,len(num)-1)
+for i in range(len(num)//2):
+    num[i],num[len(num)-1-i]=num[len(num)-1-i],num[i]
+
 print(num)
+
+        
+        
+
+
+
+
+
+
 
 
