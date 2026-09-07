2301f186a5a98adb165abd4ccde7533474ba632d
ahad waknis <ahadwaknis666@gmail.com>
2026-09-07 10:48:26 +0530

    palindrom using the recusion

--- dsa question/recursion question/palindrome _string.py
+++ dsa question/recursion question/palindrome _string.py
@@ -1,22 +1,38 @@
 """ checking the string is palidrom  or not using loop and recusion
 """
-s="mam"
 
-n=len(s)
-left=0
-right=n-1
-while left<right:
-    if s[left]!=s[right]:
-       print(False)
-       break
+#using for loop
+# s="mam"
+
+# n=len(s)
+# left=0
+# right=n-1
+# while left<right:
+#     if s[left]!=s[right]:
+#        print(False)
+#        break
     
-    left=left+1
-    right=right-1
+#     left=left+1
+#     right=right-1
 
-else:
-    print(True)
+# else:
+#     print(True)
+
+#using recursion
 
 
+def func(s,left,right):
+    if left>=right:
+        return True
+    
+    if s[left]!=s[right]:
+        return False
+
+    return func(s,left+1,right-1)
+
+
+
+print(func("jhj",0,len("jhj")-1))
 
 
 
